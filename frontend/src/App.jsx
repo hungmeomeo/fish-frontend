072357d03a12a8a3583b01c8bce9569f110a3990
Minh Hung <acubu6969@gmail.com>
2023-10-22 20:17:13 +0700

    Set router for pages, use to switch page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        {/* Add another pages here */}
+      </Routes>
+    </Router>
+
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to HOME PAGE!</h1>
+            <p>Thanks for visiting. Please take a look around.</p>
+        </div>
+    );
+};
+
+export default HomePage;
