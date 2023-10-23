236820d5286893a33f75c3ad39354cb688d77dfb
Minh Hung <acubu6969@gmail.com>
2023-10-23 13:06:24 +0700

    install tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/AuthPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage/>} />
+        <Route path="/login" element={<LoginPage/>} />
         {/* Add another pages here */}
       </Routes>
     </Router>
