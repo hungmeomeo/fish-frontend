ac05b3a969b55c2395fb0769577b8ccb517f6215
Minh Hung <acubu6969@gmail.com>
2023-10-23 13:31:07 +0700

    login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/AuthPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
