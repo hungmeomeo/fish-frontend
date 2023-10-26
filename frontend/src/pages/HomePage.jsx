9765ddffcfd10c1b5b6871aa931bd89007234f51
harrygooner <hunggtran0805@gmail.com>
2023-10-26 21:44:05 +0700

    modify package files

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -17,9 +17,11 @@ const HomePage = () => {
             <div className="pt-10 w-full h-[800px] text-center text-black">
                 <h1 className='text-[30px] font-bold'>WHAT S NEW???</h1>
                 <hr className="w-48 h-1 mx-auto my-4 bg-black border-0 rounded md:my-10 dark:bg-gray-700"></hr >
-                <p>YOUR FAVOURITE BRANDS IN ONE STORE</p>
+                <p>List of most recent products created.
+                </p>
             </div>
-            <div style={{ backgroundImage: "url('src/assets/img/fishWall.jpg')", backgroundRepeat: "no-repeat ", backgroundSize: "100% 800px" }} className="pt-25 w-full h-[800px] text-center text-white font-bold">
+            <div style={{ backgroundImage: "url('src/assets/img/fishWall.jpg')", backgroundRepeat: "no-repeat ", backgroundSize: "100% 800px" }} className=" w-full h-[800px] text-center text-white font-bold">
+                <div className='pt-25'></div>
                 <h1 className='text-[60px]'>SHOP OUR REEL COLLECTION</h1>
                 <p>YOUR FAVOURITE BRANDS IN ONE STORE</p>
             </div>
