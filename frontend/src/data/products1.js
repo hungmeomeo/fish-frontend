39d967fcf173bdfdaf81273d0182e3d493deee64
harrygooner <hunggtran0805@gmail.com>
2023-11-04 17:45:06 +0700

    What's new? homepage

--- frontend/src/data/products1.js
+++ frontend/src/data/products1.js
@@ -3,49 +3,49 @@ export const products1 = [
         id: 1,
         name: "Super Backpack",
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "src/assets/img/fish_brand.jpg"
+        image: "src/assets/img/fish_brand.png"
     },
     {
         id: 2,
         name: "New Hip",
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "src/assets/img/fish_brand.jpg"
+        image: "src/assets/img/fish_brand.png"
     },
     {
         id: 3,
         name: "Elite Series",
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "src/assets/img/fish_brand.jpg"
+        image: "src/assets/img/fish_brand.png"
     },
     {
         id: 4,
         name: "Casual",
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "src/assets/img/fish_brand.jpg"
+        image: "src/assets/img/fish_brand.png"
     },
     {
         id: 5,
         name: "Best Tote",
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "src/assets/img/fish_brand.jpg"
+        image: "src/assets/img/fish_brand.png"
     },
     {
         id: 6,
         name: "Charming Series",
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "src/assets/img/fish_brand.jpg"
+        image: "src/assets/img/fish_brand.png"
     },
 
     {
         id: 7,
         name: "Charming ",
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "src/assets/img/fish_brand.jpg"
+        image: "src/assets/img/fish_brand.png"
     },
     {
         id: 8,
         name: "Best Tote",
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "src/assets/img/fish_brand.jpg"
+        image: "src/assets/img/fish_brand.png"
     },
 ]
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -24,35 +24,21 @@ import {
     Select
 } from '@mui/material'
 import React, { useState } from 'react'
-import AppPagination from '../components/Pagnigation/AppPagination'
+// import AppPagination from '../components/Pagnigation/AppPagination'
 import { useNavigate } from "react-router-dom";
-let products1 = [
-    {
-        id: 1,
-        name: "Super Backpack",
-        price: 129.99,
-        description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "https://fishingtackledirect.ie/wp-content/uploads/2020/02/Fishing-Rods.jpg"
-    }
-]
+import { products } from "../data/products";
+import { products1 } from "../data/products1";
 const HomePage = () => {
     const [filter, setFilter] = useState('');
     const [sort, setSort] = useState('');
-    const [products, setProducts] = useState([])
     const navigate = useNavigate();
-    const handleFilter = (event) => {
-        setFilter(event.target.value);
-    };
-
-
-    const handleSort = (event) => {
-        setSort(event.target.value);
-    };
     const handleView = (id) => {
         console.log('idinlist', id);
         navigate(`/products/${id}`)
     }
-
+    let newProduct = products.slice(0, 4)
+    let newProduct1 = products1.slice(0, 4)
+    console.log(newProduct);
     console.log('product', products)
     console.log('product1', products1[0])
     return (
@@ -71,12 +57,12 @@ const HomePage = () => {
 
                     <Grid
                         container
-                        spacing={{ xs: 2, md: 3 }}
+                        spacing={{ xs: 2, md: 4 }}
                         sx={{ justifyContent: "center", alignItems: "center" }}
                         columns={{ xs: 2, sm: 4, md: 6 }}
                     >
-                        {products.map((product) => (
-                            <Grid item key={product.id} xs={2} sm={2} md={1} sx={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", margin: 0 }}>
+                        {newProduct.map((product) => (
+                            <Grid item key={product.id} xs={2} sm={2} md={1.5} sx={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", margin: 0 }}>
                                 <Box sx={{
                                     display: "flex",
                                     justifyContent: "center",
@@ -112,7 +98,7 @@ const HomePage = () => {
                             </Grid>
                         ))}
                     </Grid>
-                    <AppPagination setProducts={(p) => setProducts(p)} />
+                    {/* <AppPagination setProducts={(p) => setProducts(p)} /> */}
 
                 </Container>
             </div>
@@ -123,6 +109,54 @@ const HomePage = () => {
             <div className="pt-10 w-full h-[800px] text-center text-black">
                 <h1 className='text-[30px] font-bold'>TOP BRAND</h1>
                 <hr className="w-48 h-1 mx-auto my-4 bg-black border-0 rounded md:my-10 dark:bg-gray-700"></hr >
+                <Container>
+
+                    <Grid
+                        container
+                        spacing={{ xs: 2, md: 4 }}
+                        sx={{ justifyContent: "center", alignItems: "center" }}
+                        columns={{ xs: 2, sm: 4, md: 6 }}
+                    >
+                        {newProduct1.map((product) => (
+                            <Grid item key={product.id} xs={2} sm={2} md={1.5} sx={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", margin: 0 }}>
+                                <Box sx={{
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    flexDirection: "column",
+                                    position: "relative"
+                                }}>
+                                    <Card >
+                                        <CardMedia
+                                            component="img"
+                                            height="194"
+                                            width="80%"
+                                            image={product.image}
+                                        />
+                                        <CardContent>
+                                            <Box sx={{ display: "flex", flexDirection: 'column', justifyContent: "center", alignItems: "center" }}>
+                                                <Typography variant="body2" >
+                                                    {product.name}
+                                                </Typography>
+                                                <Typography variant='body2' >
+                                                    {product.price}
+                                                </Typography>
+                                            </Box>
+                                        </CardContent>
+                                        <CardActions sx={{ display: 'flex', justifyContent: 'flex-end' }}   >
+                                            <Button variant='contained' onClick={() => handleView(product.id)}>
+                                                View
+                                            </Button>
+
+                                        </CardActions>
+                                    </Card>
+                                </Box>
+                            </Grid>
+                        ))}
+                    </Grid>
+                    {/* <AppPagination setProducts={(p) => setProducts(p)} /> */}
+
+                </Container>
             </div>
             <div style={{ backgroundImage: "url('src/assets/img/fishWall3.jpg')", backgroundRepeat: "no-repeat ", backgroundSize: "100% 800px" }} className="w-full h-[800px] text-center text-white font-bold">
                 <h1 className='pt-10 text-[60px]'>ANGLER VIETNAM</h1>
