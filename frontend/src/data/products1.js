ba07998abef625d2d31d9f96e18fa931e1987701
hungmeomeo <hungdevhcmut@gmail.com>
2023-11-29 21:38:33 +0700

    Linking the image

--- frontend/src/data/products1.js
+++ frontend/src/data/products1.js
@@ -1,43 +1,43 @@
 export const products1 = [
-    {
-        id: 1,
-        name: "Super Backpack",
-        image: "src/assets/img/logo1.png"
-    },
-    {
-        id: 2,
-        name: "New Hip",
-        image: "src/assets/img/fish_brand.png"
-    },
-    {
-        id: 3,
-        name: "Elite Series",
-        image: "src/assets/img/logo3.webp"
-    },
-    {
-        id: 4,
-        name: "Casual",
-        image: "src/assets/img/logo4.webp"
-    },
-    {
-        id: 5,
-        name: "Best Tote",
-        image: "src/assets/img/fish_brand.png"
-    },
-    {
-        id: 6,
-        name: "Charming Series",
-        image: "src/assets/img/fish_brand.png"
-    },
+  {
+    id: 1,
+    name: "Super Backpack",
+    image: "https://drive.google.com/uc?id=1nGwMBeKLwryzFmJBHmfSrDmKZh1k2jEw",
+  },
+  {
+    id: 2,
+    name: "New Hip",
+    image: "https://drive.google.com/uc?id=1W4iMOkDjvu_fp6rFYo3BW8TELsWMr32I",
+  },
+  {
+    id: 3,
+    name: "Elite Series",
+    image: "https://drive.google.com/uc?id=1iH-WzP2Htcslrd4C_D8TlK425vo-XoeV",
+  },
+  {
+    id: 4,
+    name: "Casual",
+    image: "https://drive.google.com/uc?id=1j7uOG00dbL4pLpf3aYmWfGr62BsxONwn",
+  },
+  {
+    id: 5,
+    name: "Best Tote",
+    image: "src/assets/img/fish_brand.png",
+  },
+  {
+    id: 6,
+    name: "Charming Series",
+    image: "src/assets/img/fish_brand.png",
+  },
 
-    {
-        id: 7,
-        name: "Charming ",
-        image: "src/assets/img/fish_brand.png"
-    },
-    {
-        id: 8,
-        name: "Best Tote",
-        image: "src/assets/img/fish_brand.png"
-    },
-]
+  {
+    id: 7,
+    name: "Charming ",
+    image: "src/assets/img/fish_brand.png",
+  },
+  {
+    id: 8,
+    name: "Best Tote",
+    image: "src/assets/img/fish_brand.png",
+  },
+];
